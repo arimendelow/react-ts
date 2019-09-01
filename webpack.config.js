2ad2a8da99e863ace846c3a823319f47282b14ba
Ari Mendelow <ari@mendelow.me>
2019-09-01 13:18:22 -0500

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = requrie('webpack');
+const path = require('path');
+
+module.exports = {
+  // put sourcemaps inline
+  devtool: 'eval',
+
+  // entry point of our app, within the 'src' directory
+  entry:['index.tsx'],
+
+  // configure the output dir and publicPath for the devServer
+  output: {
+    filename: 'app.js',
+    publicPath: 'dist',
+    path: path.resolve('dist'),
+  },
+
+  // configure the dev server to run
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    inline: true,
+  },
+
+  // tell Webpack to load TS files
+  resolve: {
+    // look for modules in .ts(x) files first, then .js
+    extensions: ['.ts', '.tsx.', '.js'],
+
+    // add 'src' to the modules, so that when we import files we can do so with 'src' as the relative route
+    modules: ['src', 'node_modules'],
+  },
+
+  module: {
+    loaders: [
+      // .ts(x) files should first pass through the TS loader, and then through babel
+      {
+        test: /\.tsx?$/,
+        loaders: ['babel-loader', 'ts-loader'],
+        include: path.resolve('src'),
+      },
+    ],
+  },
+};
